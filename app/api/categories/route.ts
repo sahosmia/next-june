381be8fb63ee033e2a77bd74ab884519dd4ffaa4
sahosridoy <sahosmia.webdev@gmail.com>
2024-06-27 22:19:48 +0600

    category get request full complete

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -3,19 +3,64 @@ import { getNextId } from "@/app/types";
 import { NextRequest } from "next/server";
 
 export const dynamic = "force-dynamic";
+
 export const GET = (request: NextRequest) => {
   const searchParams = request.nextUrl.searchParams;
-  const title = searchParams.get("title");
-  if (title) {
-    const data = categories.filter((item) =>
-      item.title.toLowerCase().includes(title.toLowerCase())
+  const search = searchParams.get("search");
+  const sort = searchParams.get("sort") || "";
+  const page = parseInt(searchParams.get("page") || "1", 10);
+  const limit = parseInt(searchParams.get("limit") || "10", 10);
+
+  if (isNaN(page) || isNaN(limit)) {
+    return new Response(
+      JSON.stringify({ error: "Invalid pagination parameters" }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      }
     );
-    return Response.json(data);
   }
-  return Response.json({ data: categories, status: 200 });
+  let data = categories;
 
+  // Filter
+
+  // Search
+  if (search) {
+    data = data.filter((item) =>
+      item.title.toLowerCase().includes(search.toLowerCase())
+    );
+  }
+
+  // Short
+  if (sort) {
+    data = data.sort((a, b) => {
+      if ((a as any)[sort] < (b as any)[sort]) return -1;
+      if ((a as any)[sort] > (b as any)[sort]) return 1;
+      return 0;
+    });
+  }
+
+  // Pagination
+  const startIndex = (page - 1) * limit;
+  const paginatedData = data.slice(startIndex, startIndex + limit);
+  const total = data.length;
+  const totalPage = Math.ceil(total / limit);
+
+  // response
+  return new Response(
+    JSON.stringify({
+      total: total,
+      totalPage: totalPage,
+      page: page,
+      limit: limit,
+      data: paginatedData,
+    }),
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    }
+  );
 };
-
 
 export const POST = async (request: NextRequest) => {
   const res = await request.json(); // get request veriable
@@ -28,6 +73,6 @@ export const POST = async (request: NextRequest) => {
     headers: {
       "Content-Type": "application/json",
     },
-    status: 201, 
+    status: 201,
   });
 };
